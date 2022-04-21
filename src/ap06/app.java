753f9a0fdd1eefbb1287a32d2befccbf1e62e177
Pedro Abreu <pedrofelipeferreira1311@gmail.com>
2022-04-21 16:16:44 -0300

    atualizacao

---
+++ src/ap06/app.java
@@ -0,0 +1,10 @@
+// Olar
+package ap06;
+
+public class app{
+    public static void main(String[] args) {
+        System.out.println("olar");
+        System.out.println("sem o r");
+        System.out.println("Pedro n quer que a gente compile! :/");
+    }
+}
