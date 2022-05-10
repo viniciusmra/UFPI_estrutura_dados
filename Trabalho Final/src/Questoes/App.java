10a12f914d5c632a8b88b3404b5f5895cbe6209a
Pedro Abreu <pedrofelipeferreira1311@gmail.com>
2022-05-09 21:59:17 -0300

    Atualização Menu

--- Trabalho Final/src/Questoes/App.java
+++ Trabalho Final/src/Questoes/App.java
@@ -2,60 +2,148 @@ package Questoes;
 
 import java.io.FileNotFoundException;
 import java.util.Vector;
-
+import java.util.Scanner;
 
 public class App <Key, Value>{
 
     public static void main(String[] args) throws FileNotFoundException {
-
+		Scanner keyboard = new Scanner(System.in);
+		Integer key;
 		//Vector<String> arq = new Vector<String>();
-
 		//criando arquivo sem palavras repetidas
+		//arq = readWords("leipzig100kUnic.txt");
         // arq = readWords("src/Questoes/leipzig100k.txt");
 		// newFile(arq, "src/Questoes/leipzig100kUnic.txt");
-		//arq = readWords("leipzig100kUnic.txt");
-		
-		// //QUESTAO 1:
-		Questao1 q1 = new Questao1();
-		Vector<String> aq1Vect = new Vector<String>();
 
-		// aq1Vect.addAll(q1.timeST("leipzig100kUnic.txt"));
+		Questao1 classeQ1 = new Questao1();
+		Questao2 classeQ2 = new Questao2();
+		Questao3 classeQ3 = new Questao3();
 
-		// for(int i=0; i< aq1Vect.size(); i++){
-		// 	System.out.println(aq1Vect.get(i));
-		// }
+		Vector<String> metodosQ1 = new Vector<String>();
+		Vector<String> metodosQ2 = new Vector<String>();
+		Vector<String> metodosQ3 = new Vector<String>();
 
-		aq1Vect.addAll(q1.timeVectorST("leipzig100kUnic.txt"));
+		boolean flag = true;
+		while(flag){
 
-		//aq1Vect.addAll(q1.timeArrayListST("leipzig100kUnic.txt"));
+			System.out.println("=-=-=-=-=-=-=-=-=-=");
+			System.out.println("        MENU       ");
+			System.out.println("=-=-=-=-=-=-=-=-=-=");
+			System.out.println("[0] Classes List");
+			System.out.println("[1] Classes Set");
+			System.out.println("[2] Classes MAP");
+			System.out.println("[3] Tudo");
+			System.out.println("[4] Sair");
+			System.out.println("=-=-=-=-=-=-=-=-=-=");
+			System.out.print("Informe uma opção:");
+			key = keyboard.nextInt();
+			//keyboard.next();
+			System.out.println("=-=-=-=-=-=-=-=-=-=");
 
-		//aq1Vect.addAll(q1.timeLinkedListST("leipzig100kUnic.txt"));
-		for(int i=0; i< aq1Vect.size(); i++){
-			System.out.println(aq1Vect.get(i));
+			switch(key){
+
+				case 0:
+					System.out.println("Carregando...");
+					System.out.println("=-=-=-=-=-=-=-=-=-=");
+
+					metodosQ1.addAll(classeQ1.timeVectorST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ1.addAll(classeQ1.timeArrayListST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ1.addAll(classeQ1.timeLinkedListST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+			
+					for(int i=0; i<  metodosQ1.size(); i++){
+						System.out.println(metodosQ1.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}
+					}
+
+				break;
+
+				case 1:
+					System.out.println("Carregando...");
+					System.out.println("=-=-=-=-=-=-=-=-=-=");
+
+					metodosQ2.addAll(classeQ2.timeHashSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ2.addAll(classeQ2.timeLinkedHashSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ2.addAll(classeQ2.timeTreeSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+
+					for(int i=0; i<  metodosQ2.size(); i++){
+						System.out.println(metodosQ2.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}
+					}
+
+				break;
+
+				case 2:
+					System.out.println("Carregando...");
+					System.out.println("=-=-=-=-=-=-=-=-=-=");
+
+					metodosQ3.addAll(classeQ3.timeTreeMapST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ3.addAll(classeQ3.timeLinkedHashST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ3.addAll(classeQ3.timeHashMapST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+
+					for(int i=0; i<  metodosQ3.size(); i++){
+						System.out.println(metodosQ3.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}					}
+
+				break;
+
+				case 3:
+					System.out.println("Carregando...");
+					System.out.println("=-=-=-=-=-=-=-=-=-=");
+
+					
+					metodosQ1.addAll(classeQ1.timeVectorST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ1.addAll(classeQ1.timeArrayListST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ1.addAll(classeQ1.timeLinkedListST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+
+					metodosQ2.addAll(classeQ2.timeHashSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ2.addAll(classeQ2.timeLinkedHashSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ2.addAll(classeQ2.timeTreeSetST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));	
+
+					metodosQ3.addAll(classeQ3.timeTreeMapST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ3.addAll(classeQ3.timeLinkedHashST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+					metodosQ3.addAll(classeQ3.timeHashMapST("Trabalho Final/src/Questoes/leipzig100kUnic.txt"));
+
+					for(int i=0; i<  metodosQ1.size(); i++){
+						System.out.println(metodosQ1.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}
+					}
+					
+					for(int i=0; i<  metodosQ2.size(); i++){
+						System.out.println(metodosQ2.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}
+					}
+					
+					for(int i=0; i<  metodosQ3.size(); i++){
+						System.out.println(metodosQ3.get(i));
+						if(i%3==0){
+							System.out.println("=-=-=-=-=-=-=-=-=-=");
+						}
+					}
+
+				break;
+
+				case 4:
+					System.out.println("Encerrando...");
+					flag=false;
+				break;
+
+				default:
+					key = keyboard.nextInt();
+					keyboard.next();
+			}
 		}
-
-		// //QUESTAO 2:
-
-		// Questao2 q2 = new Questao2();
-		// Vector<String> aq2Vect = new Vector<String>();
-		// aq2Vect.addAll(q2.ST("leipzig100kUnic.txt"));
-
-		// for(int j=0; j< aq2Vect.size(); j++){
-		// 	System.out.println(aq2Vect.get(j));
-		// }
-
-		//QUESTAO 3:
-
-		// Questao3 q3 = new Questao3();
-		// Vector<String> aq3Vect = new Vector<String>();
-		// aq3Vect.addAll(q3.ST("leipzig100kUnic.txt"));
-
-		// for(int j=0; j< aq3Vect.size(); j++){
-		// 	System.out.println(aq3Vect.get(j));
-		// }
-
-		
-		
+		System.out.println("=-=-=-=-=-=-=");
+		keyboard.close();
      }
 
 }
--- Trabalho Final/src/Questoes/Questao3.java
+++ Trabalho Final/src/Questoes/Questao3.java
@@ -6,13 +6,14 @@ import java.util.LinkedHashMap;
 import java.util.HashMap;
 
 public class Questao3{
+    //TREEMAP
     public Vector<String> timeTreeMapST(String filename){
         Vector<String> arq = new Vector<String>();
         Vector<String> time = new Vector<String>();
         arq = Remove.readWords(filename);
         String[] words = {"Lisbon", "NASA", "Kyunghee","Konkuk", "Sogang", "momentarily", "rubella", "vaccinations", "government", "Authorities"};
       
-        // Inserção
+        // Insercao
 		long tmInicialTM = System.currentTimeMillis();
         
         TreeMap<String, Integer>  stTreeMapST = new TreeMap<>();
@@ -29,44 +30,64 @@ public class Questao3{
         }
 
         long tmFinalTMP = System.currentTimeMillis();
-        time.add("Search time Vector : " + (tmFinalTMP-tmInicialTMP) + " ms"); 
+        time.add("Search time Treemap : " + (tmFinalTMP-tmInicialTMP) + " ms"); 
 
         // Delete
         long tmInicialSTD = System.currentTimeMillis();
         for(int i = 0; i < words.length; i++){
             stTreeMapST.remove(words[i]);
-            System.out.println(stTreeMapST.containsKey(words[i]));
+            //System.out.println(stTreeMapST.containsKey(words[i]));
         }
         long tmFinalSTD = System.currentTimeMillis();
-        time.add("Delete time Vector : " + (tmFinalSTD-tmInicialSTD) + " ms"); 
+        time.add("Delete time Treemap : " + (tmFinalSTD-tmInicialSTD) + " ms"); 
 
         return time;
     }
-public Vector<String> ST(String filename){
+    //LINKEDHASHMAP
+    public Vector<String> timeLinkedHashST(String filename){
         Vector<String> arq = new Vector<String>();
         Vector<String> time = new Vector<String>();
         arq = Remove.readWords(filename);
-
-        //TREEMAP
-		long tmInicialTM = System.currentTimeMillis();
+        String[] words = {"Lisbon", "NASA", "Kyunghee","Konkuk", "Sogang", "momentarily", "rubella", "vaccinations", "government", "Authorities"};
         
-        TreeMap<String, Integer>  stTreeMapST = new TreeMap<>();
+        // Insercao
+		long tmInicialTM = System.currentTimeMillis();
+        LinkedHashMap<String, Integer> stLinkedHasMapST = new LinkedHashMap<String, Integer>();
         for(int x=0 ; x < arq.size(); x++){// elemento a ser comparado
-         		stTreeMapST.put(arq.get(x), null);
+            stLinkedHasMapST.put(arq.get(x), null);
 		  }		
 		long tmFinalTM = System.currentTimeMillis();
-        time.add("Treemap : " + (tmFinalTM-tmInicialTM));	
+        time.add("LinkedHash : " + (tmFinalTM-tmInicialTM));	
 
-		//LINKEDHASHMAP
-		long tmInicialLHM = System.currentTimeMillis();
-        LinkedHashMap<String, Integer> stLinkedHashMapST = new LinkedHashMap<String, Integer>();
-        for(int x=0 ; x < arq.size(); x++){// elemento a ser comparado
-				stLinkedHashMapST.put(arq.get(x), null);
-		  }		
-		long tmFinalLHM = System.currentTimeMillis();
-        time.add("LinkedHashMap : " + (tmFinalLHM-tmInicialLHM));
+         // Procura
+         long tmInicialLHMP = System.currentTimeMillis();
+         for(int i = 0; i < words.length; i++){
+            stLinkedHasMapST.containsKey(words[i]);
+        }
+
+        long tmFinalLHMP = System.currentTimeMillis();
+        time.add("Search time LinkedHash : " + (tmFinalLHMP-tmInicialLHMP) + " ms"); 
+
+        // Delete
+        long tmInicialLHMD = System.currentTimeMillis();
+        for(int i = 0; i < words.length; i++){
+            stLinkedHasMapST.remove(words[i]);
+            //System.out.println(stLinkedHasMapST.containsKey(words[i]));
+        }
+        long tmFinalLHMD = System.currentTimeMillis();
+        time.add("Delete time LinkedHash : " + (tmFinalLHMD-tmInicialLHMD) + " ms"); 
+
+        return time;
+    }
+    public Vector<String> timeHashMapST(String filename){
+        Vector<String> arq = new Vector<String>();
+        Vector<String> time = new Vector<String>();
+        String[] words = {"Lisbon", "NASA", "Kyunghee","Konkuk", "Sogang", "momentarily", "rubella", "vaccinations", "government", "Authorities"};
+        arq = Remove.readWords(filename);
 
         //HASHMAP
+
+        //inserir
 		long tmInicialHM = System.currentTimeMillis();
         HashMap<String, Integer> stHashMapST = new HashMap<String, Integer>();
         //TreeSet<String> stTreeMapST = new TreeSet<String>();
@@ -75,6 +96,24 @@ public Vector<String> ST(String filename){
 		  }		
 		long tmFinalHM = System.currentTimeMillis();
         time.add("HashMap : " + (tmFinalHM-tmInicialHM));
+
+        // Procura
+        long tmInicialHMP = System.currentTimeMillis();
+        for(int i = 0; i < words.length; i++){
+            stHashMapST.containsKey(words[i]);
+        }
+
+        long tmFinalHMP = System.currentTimeMillis();
+        time.add("Search time HashMap : " + (tmFinalHMP-tmInicialHMP) + " ms"); 
+
+        // Delete
+        long tmInicialHMD = System.currentTimeMillis();
+        for(int i = 0; i < words.length; i++){
+            stHashMapST.remove(words[i]);
+            //System.out.println( stHashMapST.containsKey(words[i]));
+        }
+        long tmFinalHMD = System.currentTimeMillis();
+        time.add("Delete time HashMap : " + (tmFinalHMD-tmInicialHMD) + " ms"); 
 
         return time;
     }
